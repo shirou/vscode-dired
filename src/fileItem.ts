bc8d0a3929d15bb45f791df2a4adc1b9ac2b749a
Satoshi Noguchi <sanoguch@gmail.com>
2020-03-09 21:49:47 +0900

    Possible bug fix to get directory URI for Windows.

--- src/fileItem.ts
+++ src/fileItem.ts
@@ -110,7 +110,8 @@ export default class FileItem {
     public get uri(): vscode.Uri | undefined {
         const p = path.join(this._dirname, this._filename);
         if (this._isDirectory) {
-            return vscode.Uri.parse(`${DiredProvider.scheme}://${p}`);
+            const u = new URL(`${DiredProvider.scheme}://${p}`);
+            return vscode.Uri.parse(u.href);
         } else if (this._isFile) {
             const u = new URL(`file:///${p}`);
             return vscode.Uri.parse(u.href);
